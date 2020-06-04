c15572fc2cb7f5db8b16586c6c2027bb101ba916
priyanka10it <priyanka10.it@gmail.com>
2020-06-03 23:29:14 -0700

    Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Switch } from 'react-router-dom';
 
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
 
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+    const [savedList, setSavedList] = useState([]);
+    const [movieList, setMovieList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
+    useEffect(() => {
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+        const getMovies = () => {
+            axios
+                .get('http://localhost:5000/api/movies')
+                .then(response => {
+                    setMovieList(response.data);
+                })
+                .catch(error => {
+                    console.error('Server Error', error);
+                });
+        }
+
+        getMovies();
+
+    }, []);
+
+    const addToSavedList = movie => {
+
+        setSavedList([...savedList, movie]);
+
+    };
+
+    return (
+
+        <div>
+
+            <SavedList list={savedList} />
+
+            <Switch>
+
+                <Route exact path = "/"  >
+                    <MovieList movies = {movieList} />
+                </Route>
+
+                <Route path='/movies/:id'>
+                    <Movie movies = {movieList} addToSavedList = {addToSavedList}/>
+                </Route>
+
+            </Switch>
+        </div>
+
+    );
+
 };
 
 export default App;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,35 +1,21 @@
 import React from 'react';
+import MovieCard from './MovieCard';
+import { Link } from 'react-router-dom';
 
-const MovieList = props => {
+export default function MovieList(props){
+
   return (
+
     <div className="movie-list">
+
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to ={`/movies/${movie.id}`}>
+          <MovieCard key={movie.id} movie={movie} />
+        </Link>
+
       ))}
+
     </div>
+
   );
 }
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default MovieList;
